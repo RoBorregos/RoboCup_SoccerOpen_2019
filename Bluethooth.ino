4808d105adecacf72090bfff45f3f244eb8b4539
Mauricio Juárez Ibáñez <44931871+zJuarez@users.noreply.github.com>
2019-03-13 16:04:50 -0600

    Delete Bluethooth.ino

--- Bluethooth.ino
+++
@@ -1,7 +0,0 @@
-  string message;
-  void verMensaje (){
-    
-if(Serial3.available() > 0){ // Checks whether data is comming from the serial port
-    message = Serial3.read(); // Reads the data from the serial port
- }
-  }
